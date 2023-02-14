06ef79267ec35e5227ead924611d3fd03b0e6161
TheoR <reynier.theo@gmail.com>
2023-02-14 16:24:42 +0100

    Concept de base.html, css ...

--- serveur.py
+++ serveur.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title='Accueil'
+    return render_template("index.html",title=title)
+
+@app.route("/listeTableaux")
+def listeTableaux():
+    title='Gallerie'
+    return render_template("lTab.html",title=title)
+
+@app.route("/contact")
+def contact():
+    title='Contact'
+    return render_template("contact.html",title=title)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
